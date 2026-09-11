624cc9d6de9da3191696cb23b6879ef4cab8e4ea
ameliend <amelien.delahaie@gmail.com>
2026-09-11 22:46:31 +0200

    Fix search icon rendering

--- user-menu.js
+++ user-menu.js
@@ -5,7 +5,8 @@
     searchButton.className = 'search';
     searchButton.type = 'button';
     searchButton.setAttribute('aria-label', 'Rechercher');
-    searchButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-hidden="true" class="size-dt-size-24 text-dt-button-icon-icon-ghost-tertiary-default group-hover:text-dt-button-icon-icon-ghost-tertiary-hover group-active:text-dt-button-icon-icon-ghost-tertiary-active"><path fill="currentColor" fill-rule="evenodd" d="M17.762 14.07a5.52 5.52 0 0 0-3.917 1.622 5.539 5.539 0 1 0 3.917-1.623m1.821-9.653A8.2 8.2 0 0 0 13.75 2a8.2 8.2 0 0 0-5.834 2.417 8.2 8.2 0 0 0-2.417 5.835 8.2 8.2 0 0 0 1.527 4.78l-4.623 4.624a1.372 1.372 0 0 0 1.94 1.941l4.625-4.623a8.2 8.2 0 0 0 4.78 1.526 8.2 8.2 0 0 0 5.836-2.415A8.2 8.2 0 0 0 22 10.25a8.2 8.2 0 0 0-2.417-5.834" clip-rule="evenodd"></path><path fill="currentColor" fill-rule="evenodd" d="M17.762 14.07a5.52 5.52 0 0 0-3.917 1.622 5.539 5.539 0 1 0 3.917-1.623m1.821-9.653A8.2 8.2 0 0 0 13.75 2a8.2 8.2 0 0 0-5.834 2.417 8.2 8.2 0 0 0-2.417 5.835 8.2 8.2 0 0 0 1.527 4.78l-4.623 4.624a1.372 1.372 0 0 0 1.94 1.941l4.625-4.623a8.2 8.2 0 0 0 4.78 1.526 8.2 8.2 0 0 0 5.836-2.415A8.2 8.2 0 0 0 22 10.25a8.2 8.2 0 0 0-2.417-5.834" clip-rule="evenodd"></path></svg>';
+    searchButton.style.cssText = 'width:24px;height:24px;padding:0;border:0;background:transparent;color:inherit;cursor:pointer';
+    searchButton.innerHTML = '<img src="./search-icon.svg" alt="" width="24" height="24">';
     searchIcon.replaceWith(searchButton);
   }
   const avatarIcon = document.querySelector('.avatar');
